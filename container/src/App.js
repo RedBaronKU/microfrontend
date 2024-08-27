b83a7b5fc2ec839c69bff70f87f09d604f496174
utkarsh.kumar <utkarsh.kumar@lysto.io>
2024-08-27 10:26:04 +0530

    Routing and styles added

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import ErrorBoundary from "./ErrorBoundary";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi There container</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <ErrorBoundary>
+            <MarketingApp />
+          </ErrorBoundary>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(pathname);
+      }
+    },
+  };
 };
 
 // Check if we are in development mode and if the element exists
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.getElementById("marketing_dev_root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
