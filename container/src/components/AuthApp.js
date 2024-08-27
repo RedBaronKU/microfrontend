fc254d480ae43e95bdc978ab989330c7aa33798c
utkarsh.kumar <utkarsh.kumar@lysto.io>
2024-08-27 14:46:33 +0530

    Adding auth and sync memory History with Container

--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -1,4 +1,4 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
@@ -11,9 +11,13 @@ export default () => {
       initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
-        if (pathname != nextPathname) history.push(nextPathname);
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
       },
     });
+
     history.listen(onParentNavigate);
   }, []);
 
